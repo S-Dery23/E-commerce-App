3f613458323b0aefea334e723b1ad18bee699b12
Seth Sangber-Dery <90282197+S-Dery23@users.noreply.github.com>
2021-11-02 10:40:08 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,72 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { TouchableOpacity, KeyboardAvoidingView, StyleSheet, Text, View, TextInput, ShadowPropTypesIOS, ToastAndroid, Button } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import loginScreen from './screens/loginScreen';
+import homeScreen from './screens/homeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-
   return (
-    <View style={styles.container}>
-      <View style={styles.flex}>
-      <Text style={styles.flexHead}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='login'>
+        <Stack.Screen name='login' component={loginScreen} />
+        <Stack.Screen name='home' component={homeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-      <View style={styles.hll}>
-      <View style={styles.flex}>
-        <Text style={styles.flexText}>Email</Text>
-        <TextInput value={email} onChangeText={setEmail} style={styles.insert}/>
-      </View>
-
-      <View style={styles.flex}>
-        <Text style={styles.flexText}>Password</Text>
-        <TextInput value={password} onChangeText={setPassword} style={styles.insert}/>
-      </View>
-      </View>
-
-      <View style={styles.flex}>
-        <Button title='Login'/>
-      </View>
-    </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2b4540',
-  },
-
-  flex: {
-    margin: 5,
-    padding: 10,
-    marginHorizontal: '15%',
-    borderWidth: 1,
-    borderColor: 'red',
-    borderRadius: 10,
-    backgroundColor: '#452b30',
-  },
-
-  hll: {
-    marginVertical: '10%',
-  },
-
-  flexHead: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-
-  flexText:  {
-    fontSize: 15,
-    color: 'white',
-    fontWeight: 'bold',
-    margin: 5,
-  },
-
-  insert: {
-    padding: 5,
-  },
-
-});
+const styles = StyleSheet.create({})
