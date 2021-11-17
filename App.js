7f2e3abd303ca7b66def494a427e6e09d30082c6
Seth Sangber-Dery <90282197+S-Dery23@users.noreply.github.com>
2021-11-17 16:29:05 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
+import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import loginScreen from './screens/loginScreen';
 import homeScreen from './screens/homeScreen';
 
+import cart from './screens/cart';
+import productDetails from './screens/productDetails';
+
 const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='login'>
-        <Stack.Screen name='login' component={loginScreen} />
-        <Stack.Screen name='home' component={homeScreen} />
+      <Stack.Navigator>
+        <Stack.Screen options= {{headerShown: false}} name="Log in" component={loginScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Home" component={homeScreen} />
+        
+        <Stack.Screen options= {{headerShown: false}} name="cart" component={cart} />
+        <Stack.Screen options= {{headerShown: false}} name="Details" component={productDetails} />
       </Stack.Navigator>
     </NavigationContainer>
-
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const StyleSheet = StyleSheet.create({
+  container: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
